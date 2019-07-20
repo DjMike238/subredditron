d5ca50190871c231002900beea49b7010c4774bf
Michele Dimaggio <mike238dimaggio@gmail.com>
2019-07-20 13:10:06 +0200

    Add main.go

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"gitlab.com/NicoNex/echotron"
+)
+
+
+type bot struct {
+	chatId int64
+	echotron.Engine
+}
+
+
+func NewBot(engine echotron.Engine, chatId int64) echotron.Bot {
+	return &bot{
+		chatId,
+		engine,
+	}
+}
+
+
+func (b *bot) Update(update *echotron.Update) {
+	if update.Message.Text == "/start" {
+		b.SendMessageOptions("Welcome to *Subredditron*!\nSend me any message with a subreddit in the format `r/subreddit` or `/r/subreddit` and I'll send you a link for that subreddit.", b.chatId, echotron.PARSE_MARKDOWN)
+	} else if strings.Index(update.Message.Text, "r/") != -1 && strings.Index(update.Message.Text, "reddit.com") == -1 {
+		b.SendMessageReply(subreddit(update.Message.Text), b.chatId, update.Message.ID)
+	}
+}
+
+
+func subreddit(message string) string {
+	re := regexp.MustCompile(`r\/[a-zA-Z]*`)
+	sub := re.FindString(message)
+
+	return fmt.Sprintf("https://www.reddit.com/%s", sub)
+}
+
+
+func main() {
+	echotron.RunDispatcher("TOKEN", NewBot)
+}
